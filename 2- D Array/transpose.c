d0250bbaf7aa6a6cf3605fd7ca66dffb089d92a5
sitaram-bhai <usbdon9800@gmail.com>
2023-04-14 21:37:48 +0545

    transpose

--- 2- D Array/transpose.c
+++ 2- D Array/transpose.c
@@ -14,23 +14,14 @@ int main()
             scanf("%d", &mat1[i][j]);
         }
     }
-    printf("\n");
-    for (i = 0; i < row; i++)
-    {
-        for (j = 0; j < col; j++)
-        {
-            printf("Enter %d row and %d column: ", i, j);
-            scanf("%d", &mat2[i][j]);
-        }
-    }
 
     // displaying
     printf("\nDisplaying a sum of two matrices:\n");
-    for (i = 0; i < row; i++)
+    for (i = 0; i < col; i++)
     {
-        for (j = 0; j < col; j++)
+        for (j = 0; j < row; j++)
         {
-            printf("%d\t", mat1[i][j] + mat2[i][j]);
+            printf("%d\t", mat1[j][i]);
         }
         printf("\n");
     }
