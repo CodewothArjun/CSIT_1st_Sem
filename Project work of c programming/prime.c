3aaf658c2ed4145404465b0349f93d20e1b727ff
CodeWothArjun <dolkhaliarjun9800@gmail.com>
2023-05-16 07:44:00 +0545

    exam practice

--- Project work of c programming/prime.c
+++ Project work of c programming/prime.c
@@ -2,14 +2,27 @@
 // defining and declaring
 int isprime(int n)
 {
+    int i;
+    for (i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main()
 {
-    int n;
+    int n, i;
     printf("Enter a number: ");
     scanf("%d", &n);
-    
-
-    isprime(n); // call
+    for (i = 2; i <= n; i++)
+    {
+        if (isprime(i))
+        {
+            printf("%d ", i);
+        }
+    }
 }
