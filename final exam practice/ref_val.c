fab48110158ac607a93e4f3e93fe4cc3fab6bfc1
CodeWothArjun <dolkhaliarjun9800@gmail.com>
2023-06-07 14:36:39 +0545

    new

---
+++ final exam practice/ref_val.c
@@ -0,0 +1,12 @@
+// Documentation
+/*
+Author: Arjun Rokka
+Date: 2080/02/24
+Purpose: Describe the difference between passing arguments by value and passing arguments by address with suitable program.
+*/
+
+// Preprocessor Directive
+#include <stdio.h>
+int main()
+{
+}
