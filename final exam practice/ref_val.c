6cef6462352c2e7e6e6b1b1124e6be6b9718ff71
CodeWothArjun <dolkhaliarjun9800@gmail.com>
2023-06-08 05:58:40 +0545

    finla

--- final exam practice/ref_val.c
+++ final exam practice/ref_val.c
@@ -7,6 +7,32 @@ Purpose: Describe the difference between passing arguments by value and passing 
 
 // Preprocessor Directive
 #include <stdio.h>
+
+// Global Declaration
+void value(int);
+void reference(int *);
+int val, ref;
+// Main function
 int main()
 {
+    val = 5;
+    printf("Before Function calling: %d\n\n\n", val);
+    value(val);
+    printf("After increment Value number: %d\n", val);
+
+    reference(&val);
+    printf("After increment reference number: %d\n", val);
+}
+
+// User Define function
+void value(int va)
+{
+    va++;
+    printf("Inside Value number: %d\n", va);
+}
+
+void reference(int *fe)
+{
+    (*fe)++;
+    printf("Inside Refrence number: %d\n", *fe);
 }
